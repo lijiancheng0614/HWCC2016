2e9a5c6f377e932a89f5840c9a70ae0310f98065
lijiancheng <lijiancheng0614@gmail.com>
2016-03-21 17:25:29 +0800

    Add greedy solution.

--- SDK-gcc/future_net/route.cpp
+++ SDK-gcc/future_net/route.cpp
@@ -12,6 +12,7 @@ typedef pair<int, int> pii;
 
 int startTime;
 bool demand[N];
+vi demandVec;
 
 struct graph
 {
@@ -76,6 +77,7 @@ struct Solver
         ans = 252645135;
         s = 0;
         memset(b, 0, sizeof(b));
+        memset(v, 0, sizeof(v));
     }
     void add(int i, int j, int k, int l)
     {
@@ -187,6 +189,62 @@ struct Solver
         }
         return d[t];
     }
+    void greedyGetPath(int x, int y)
+    {
+        stack<int> pathStack;
+        v[x] = 1;
+        for (int i = y; i != x; i = p[i][0])
+        {
+            v[i] = 1;
+            pathStack.push(p[i][1]);
+        }
+        while (!pathStack.empty())
+        {
+            path.push_back(pathStack.top());
+            pathStack.pop();
+        }
+    }
+    void greedy(int k)
+    {
+        int tot = (int)demandVec.size();
+        while (1)
+        {
+            if (time(NULL) - startTime > 9)
+                return;
+            random_shuffle(demandVec.begin(), demandVec.end());
+            memset(v, 0, sizeof(v));
+            spfa(k);
+            int next = demandVec[0];
+            int cost = d[next];
+            if (cost + revGraph.d[next] + tot - 3 >= ans)
+                continue;
+            path.clear();
+            greedyGetPath(k, next);
+            for (int i = 1; i < tot; ++i)
+                if (!v[demandVec[i]])
+                {
+                    spfa(next);
+                    cost += d[demandVec[i]];
+                    if (cost + revGraph.d[demandVec[i]] >= ans)
+                    {
+                        cost = ans;
+                        break;
+                    }
+                    greedyGetPath(next, demandVec[i]);
+                    next = demandVec[i];
+                }
+            if (cost >= ans)
+                continue;
+            spfa(next);
+            cost += d[t];
+            if (cost < ans)
+            {
+                ans = cost;
+                greedyGetPath(next, t);
+                pathBest = path;
+            }
+        }
+    }
     void output()
     {
         for (int i = 0; i < (int)pathBest.size(); ++i)
@@ -202,20 +260,28 @@ void search_route(vector<vi> topo, vi demandVector)
 {
     // topo[i]: LinkID, SourceID, DestinationID, Cost
     startTime = time(NULL);
+    int edgenum = (int)topo.size();
+    int s = demandVector[0];
+    int t = demandVector[1];
+    int tot = demandVector.size();
     sort(topo.begin(), topo.end(), cmp);
     g.init();
-    for (int i = 0; i < (int)topo.size(); ++i)
+    for (int i = 0; i < edgenum; ++i)
     {
         g.add(topo[i][1], topo[i][2], topo[i][3], topo[i][0]);
         revGraph.add(topo[i][2], topo[i][1], topo[i][3]);
     }
-    int s = demandVector[0];
-    g.t = demandVector[1];
-    int tot = demandVector.size();
+    g.t = t;
     memset(demand, 0, sizeof(demand));
     for (int i = 2; i < tot; ++i)
+    {
         demand[demandVector[i]] = 1;
+        demandVec.push_back(demandVector[i]);
+    }
     revGraph.spfa(g.t);
-    g.astar(s, tot - 2, 0);
+    if (edgenum > 400)
+        g.greedy(s);
+    else
+        g.astar(s, tot - 2, 0);
     g.output();
 }
